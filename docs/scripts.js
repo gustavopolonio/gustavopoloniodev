ecd7b960577d05c258a95904ae99de9a430153d2
Gustavo <gustavopolonio1@gmail.com>
2022-08-20 11:59:14 -0300

    overlay

--- docs/scripts.js
+++ docs/scripts.js
@@ -14,19 +14,30 @@ const $menuHambIcon = document.querySelector(".menu-hamb-icon")
 const $closeMenuHamb = document.querySelector(".close-menu-hamb")
 const $menuHambItems = document.querySelector(".header-navigation-items")
 const $toggleMenuHambButton = document.querySelectorAll(".toggle-menu-hamb")
+const $overlayContainer = document.querySelector(".overlay-container")
 
 $toggleMenuHambButton.forEach(button => button.addEventListener("click", () => {
   $menuHambItems.classList.toggle("show")
   $menuHambIcon.classList.toggle("d-none")
   $closeMenuHamb.classList.toggle("d-block")
+  $overlayContainer.classList.toggle("active")
 }))
 
 // Close menu hamb
+function onCloseMenuHamb() {
+  $menuHambItems.classList.remove("show")
+  $menuHambIcon.classList.remove("d-none")
+  $closeMenuHamb.classList.remove("d-block")
+  $overlayContainer.classList.remove("active")
+}
+
 $menuHambNavigationItems = document.querySelectorAll(".navigation-item")
 $menuHambNavigationItems.forEach(item => {
   item.addEventListener("click", () => {
-    $menuHambItems.classList.remove("show")
-    $menuHambIcon.classList.remove("d-none")
-    $closeMenuHamb.classList.remove("d-block")
+    onCloseMenuHamb()
   })
+})
+
+$overlayContainer.addEventListener("click", () => {
+  onCloseMenuHamb()
 })
